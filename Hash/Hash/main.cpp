57cc502948c6dd217f4afdc990656488b332cb83
RohanSoni-02 <soni.rohanchd@gmail.com>
2024-02-17 22:02:51 +1100

    added node class and hashmap

--- Hash/Hash/main.cpp
+++ Hash/Hash/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+class Node{
+public:
+    string key;
+    int value;
+    Node* next;
+    
+    Node(string key, int value){
+        this->key = key;
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+class HashTable{
+private:
+    static const int SIZE = 7;
+    Node* dataMap[SIZE];
+};
