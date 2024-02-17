35ba7d75f5eb8122aa1c3a1c792f3a101acea5ec
RohanSoni-02 <soni.rohanchd@gmail.com>
2024-02-17 21:46:18 +1100

    initial commit

---
+++ Hash/Hash/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hash
+//
+//  Created by Rohan Soni on 17/2/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
